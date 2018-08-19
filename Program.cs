60cc83be1778fe4ddb25aa4ace28f197201d8e10
Ming <mingtungwong@outlook.com>
2018-08-18 21:03:47 -0400

    Simple code for accessing the pokemon api

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace pokesharp
 {
@@ -6,7 +9,20 @@ namespace pokesharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://pokeapi.co/api/v2/");
+            int pokemonId = 58;
+            HttpResponseMessage response = client.GetAsync($"pokemon/{pokemonId}").Result;
+            HttpResponseMessage response2 = client.GetAsync($"pokemon-species/{pokemonId}").Result;
+            if(response.IsSuccessStatusCode && response2.IsSuccessStatusCode) {
+                string respString = response.Content.ReadAsStringAsync().Result;
+                string respString2 = response2.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject<object>(respString);
+                dynamic json2 = JsonConvert.DeserializeObject<object>(respString2);
+                Pokemon p = PokemonUtils.createPokemonObject(json, json2);
+                Console.WriteLine(p.Name);
+                Console.WriteLine(p.Description);
+            } else Console.WriteLine("I got an error");
         }
     }
 }
