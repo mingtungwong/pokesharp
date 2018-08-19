85a9bc7a95ac885b74ceb66b368d467e58e2832c
Ming <mingtungwong@outlook.com>
2018-08-19 13:04:46 -0400

    Simple loop logic

--- Program.cs
+++ Program.cs
@@ -7,12 +7,37 @@ namespace pokesharp
 {
     class Program
     {
+        private static PokemonAPIClient client = new PokemonAPIClient();
+        private static bool doLoop = true;
         static void Main(string[] args)
         {
-            PokemonAPIClient client = new PokemonAPIClient();
-            Pokemon test = client.getPokemon("77");
-            Console.WriteLine(test.Name);
-            Console.WriteLine(test.Types[0]);
+            showInstructions();
+            while(doLoop) {
+                loop();
+            }
+            Console.WriteLine("Thanks for using the app!");
+        }
+
+        private static void showInstructions() {
+            Console.WriteLine("This console application calls the API at pokeapi.co to fetch Pokemon information.");
+            Console.WriteLine("Enter a Pokemon ID number or name to get information for that Pokemon. Example: 8 or charmeleon.");
+            Console.WriteLine("Enter 'e' or 'exit' to exit.");
+        }
+
+        private static void loop() {
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+            if(choice == "e" || choice == "exit") {
+                doLoop = false;
+                return;
+            }
+            Pokemon p = client.getPokemon(choice);
+            if(p == null) {
+                Console.WriteLine("Sorry, there was an error fetching the Pokemon. Your entry might have been incorrect. Please try again.");
+                return;
+            } else {
+                p.printPokedexEntry();
+            }
         }
     }
 }
